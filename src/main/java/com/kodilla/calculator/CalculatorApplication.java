04ee7cd6a9af737c536a5dc2b7e33c3a50c4f667
Szymon Wizdorf <szymekwizdorf@gmail.com>
2021-10-30 16:56:38 +0200

    Second calculator commit

--- src/main/java/com/kodilla/calculator/CalculatorApplication.java
+++ src/main/java/com/kodilla/calculator/CalculatorApplication.java
@@ -6,8 +6,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CalculatorApplication.class, args);
+    public int addNumbers(int number1, int number2){
+        int sum= number1+number2;
+        return sum;
+    }
+
+    public int subtractNumber(int number1, int number2){
+        int difference= number1-number2;
+        return difference;
+    }
+
+    public static void main(String[] args)
+        {SpringApplication.run(CalculatorApplication.class, args);
+        int sum, difference;
+        CalculatorApplication calculator= new CalculatorApplication();
+        sum=calculator.addNumbers(56,78);
+        System.out.println(sum);
+        difference=calculator.subtractNumber(123,24);
+        System.out.println(difference);
     }
 
 }
